5dc4b39dbd9d09257f7da9a5c8d7370077ba3136
Laura Fideler <laura@kintent.com>
2022-09-20 12:46:46 -0400

    such a long fine

--- src/App.js
+++ src/App.js
@@ -21,6 +21,277 @@ function App() {
       </header>
       <AmplifySignOut />
     </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>  <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>  <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       here is a change to review!
+        </p>
+        <a
+          className="App-linkss"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut />
+    </div>
+    
   );
 }
 
